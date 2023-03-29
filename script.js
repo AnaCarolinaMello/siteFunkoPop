2dd29db093354f6502144e2c435fb5c027a472d7
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2023-03-28 22:10:40 -0300

    Limpa os inputs quando os botões são clicados
    Os inputs são limpados quando seus respectivos botões são acionados

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
+let pesquisar = document.querySelector("#pesquisar");
+let pesquisa = document.querySelector("#pesquisa");
+let buttonNewsLetter1 = document.querySelector("#buttonNewsLetter1")
+let inputNewsLetter1 = document.querySelector("#inputNewsLetter1")
+let buttonNewsLetter2 = document.querySelector("#buttonNewsLetter2")
+let inputNewsLetter2 = document.querySelector("#inputNewsLetter2")
+let botaoBusca = document.querySelector("#botaoBusca")
 let selectEstados = document.querySelector("#estados")
+let selectTipos = document.querySelector("#tipos");
+let selectCategorias = document.querySelector("#categorias");
+let inputDe = document.querySelector("#inputDe")
+let inputAte = document.querySelector("#inputAte")
 
 fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(data=>{
     if(data){
@@ -11,4 +22,24 @@ fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(data=>
     update.forEach(value => {
         selectEstados.options[selectEstados.options.length] = new Option(value.nome, value.sigla);
     });
+})
+
+pesquisar.addEventListener("click",()=>{
+    pesquisa.value = ""
+});
+
+buttonNewsLetter1.addEventListener("click",()=>{
+    inputNewsLetter1.value = ""
+})
+
+buttonNewsLetter2.addEventListener("click",()=>{
+    inputNewsLetter2.value = ""
+})
+
+botaoBusca.addEventListener("click",()=>{;
+    selectTipos.value = "tipo"
+    selectCategorias.value = "categoria"
+    selectEstados.value = "estado"
+    inputDe.value = ""
+    inputAte.value = ""
 })
